542914e252bee640d54f42c1db3dfcd319999204
Sam Maus <samueldmaus@gmail.com>
2020-08-07 16:56:26 -0500

    server up and running on localhost 5000

---
+++ server/modules/pool.js
@@ -0,0 +1,7 @@
+const pg = require('pg');
+
+const config = {
+    database: 'weekend-to-do-app',
+    host: 'localhost',
+    port: 5432
+};
---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 5000;
+const tasksRouter = require('./routes/tasks.router.js')
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('server/public'));
+
+// ROUTES
+// app.use('/tasks', tasksRouter);
+
+app.listen(PORT, () => {
+    console.log('listen on port:', PORT);
+});
